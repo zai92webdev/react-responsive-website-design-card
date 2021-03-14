00fabb0f5438fab5fa40d3c291ae47482c829606
Zai <jaiminotai@gmail.com>
2021-03-14 21:26:16 +0800

    second commit

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -6,7 +6,7 @@ function Privacy() {
         <div className='privacy'>
             <header>
             <h1>UI Design Privacy Policy</h1>
-            <p>
+            <p className='privacy__paragraph'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </p>
             </header>
@@ -16,7 +16,7 @@ function Privacy() {
                 Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
                 </h2>
 
-                <p>
+                <p className='privacy__paragraph'>
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
                 </p>
 
@@ -29,13 +29,13 @@ function Privacy() {
                     <li>m, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam</li>
                 </ul>
 
-                <p>
+                <p className='privacy__paragraph'>
                 unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
                 </p>
 
                 <h4>a ) iste natus error sit voluptatem </h4>
 
-                <p>
+                <p className='privacy__paragraph'>
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui
                 </p>
 
@@ -51,13 +51,13 @@ function Privacy() {
                 1914 translation by H. Rackham
                 </h2>
 
-                <p>
+                <p className='privacy__paragraph'>
                 On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to 
                 </p>
             </div>
 
         </div>
-    )
-}
+    
+)}
 
 export default Privacy
--- src/components/Terms.js
+++ src/components/Terms.js
@@ -6,26 +6,26 @@ function Terms() {
         <div className='terms'>
             <header>
                 <h1>UI Design Terms & Conditions</h1>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is tha (" dw ")</p>
+                <p className='terms__paragraph'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is tha (" dw ")</p>
             </header>
 
             <div className='terms__contents'>
                 <h2>
                 1. Acceptance of Terms
                 </h2>
-                <p>
+                <p className='terms__paragraph'>
                     1.1. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     1.2.  omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     1.3. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     1.4. denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     1.5. On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best,
                 </p>
 
@@ -33,10 +33,10 @@ function Terms() {
                 <h2>
                 2. Newsletter Subscription
                 </h2>
-                <p>
+                <p className='terms__paragraph'>
                     2.1. denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     2.2. On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best,
                     </p>
 
@@ -44,16 +44,16 @@ function Terms() {
                 <h2>
                 3. Contests, Offers and Promotions
                 </h2>
-                <p>
+                <p className='terms__paragraph'>
                     3.1. denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     3.2. On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best,
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     3.3. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                 </p>
-                <p>
+                <p className='terms__paragraph'>
                     3.4.  omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
                 </p>
 
